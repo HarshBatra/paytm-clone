2e0dfdd4777ff3ef27602c4783c5b57a49cc4a2f
HARSH BATRA <harshbat22@gmail.com>
2024-02-14 22:07:11 +0530

    mongoose setup && user schema created

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
-const PORT = 3000;
+dotenv.config();
+app.use(express.json());
+
+//database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("database is connected successfully!");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 app.get("/", (req, res) => {
   res.json("Server is up and running");
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
+  connectDB();
   console.log("Server is running on port: " + PORT);
 });
